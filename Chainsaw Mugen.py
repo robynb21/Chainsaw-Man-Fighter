6938a44d97c2c2ec277ac7bca530f157ee7a3203
robynb21 <121036738+robynb21@users.noreply.github.com>
2023-05-16 17:34:28 -0400

    Add files via upload

--- Chainsaw Mugen.py
+++ Chainsaw Mugen.py
@@ -47,40 +47,40 @@ def options():
         p1_select = False
         p2_select = False
         char_select = False
-        
+
         AKI_ICON = Button(image=pygame.image.load("assets/aki sprite.png"), pos=(640, 260),
-                          text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
+                              text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
         AKI_ICON.changeColor(OPTIONS_MOUSE_POS)
         AKI_ICON.update(SCREEN)
 
         AKI_SELECT = Button(image=None, pos=(640, 350), 
-                            text_input="AKI", font=get_font(28), base_color="Black", hovering_color="Green")
+                                text_input="AKI", font=get_font(28), base_color="Black", hovering_color="Green")
 
         AKI_SELECT.changeColor(OPTIONS_MOUSE_POS)
         AKI_SELECT.update(SCREEN)
 
         DENJI_ICON = Button(image=pygame.image.load("assets/denji sprite.png"), pos=(320, 260),
-                          text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
+                              text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
         DENJI_ICON.changeColor(OPTIONS_MOUSE_POS)
         DENJI_ICON.update(SCREEN)
 
         DENJI_SELECT = Button(image=None, pos=(320, 350), 
-                            text_input="DENJI", font=get_font(28), base_color="Black", hovering_color="Green")
+                                text_input="DENJI", font=get_font(28), base_color="Black", hovering_color="Green")
 
         DENJI_SELECT.changeColor(OPTIONS_MOUSE_POS)
         DENJI_SELECT.update(SCREEN)
 
         POWER_ICON = Button(image=pygame.image.load("assets/power sprite.png"), pos=(960, 260),
-                          text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
+                              text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
         POWER_ICON.changeColor(OPTIONS_MOUSE_POS)
         POWER_ICON.update(SCREEN)
 
         POWER_SELECT = Button(image=None, pos=(960, 350), 
-                            text_input="POWER", font=get_font(28), base_color="Black", hovering_color="Green")
+                                text_input="POWER", font=get_font(28), base_color="Black", hovering_color="Green")
 
         POWER_SELECT.changeColor(OPTIONS_MOUSE_POS)
         POWER_SELECT.update(SCREEN)
-        
+            
         OPTIONS_TEXT = get_font(40).render("CHOOSE YOUR CHARACTERS.", True, "Black")
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(midtop=(640, 50))
         
@@ -99,8 +99,22 @@ def options():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     main_menu()
+                if DENJI_SELECT.checkForInput(OPTIONS_MOUSE_POS):
+                    DEN_CHOOSE_TEXT = get_font(40).render("PLAYER 1: DENJI", True, "Black")
+                    DEN_CHOOSE_RECT = DEN_CHOOSE_TEXT.get_rect(midbottom=(640, 600))
+                    p1_select == True
+                if AKI_SELECT.checkForInput(OPTIONS_MOUSE_POS):
+                    AKI_CHOOSE_TEXT = get_font(40).render("PLAYER 1: AKI", True, "Black")
+                    AKI_CHOOSE_RECT = AKI_CHOOSE_TEXT.get_rect(midbottom=(640, 600))
+                    p1_select == True
+                if POWER_SELECT.checkForInput(OPTIONS_MOUSE_POS):
+                    POW_CHOOSE_TEXT = get_font(40).render("PLAYER 1: POWER", True, "Black")
+                    POW_CHOOSE_RECT = POW_CHOOSE_TEXT.get_rect(midbottom=(640, 600))                        
+                    p1_select == True
 
         pygame.display.update()
+                        
+
 
 def main_menu():
     while True:
