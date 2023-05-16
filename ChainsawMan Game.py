f265039c2c2cff8ebf0fdff26fab9a2ec280defa
robynb21 <121036738+robynb21@users.noreply.github.com>
2023-05-15 22:26:30 -0400

    Add files via upload

--- ChainsawMan Game.py
+++ ChainsawMan Game.py
@@ -39,12 +39,29 @@ def play():
     
 def options():
     while True:
+        
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
         SCREEN.fill("white")
+        
+        AKI_ICON = Button(image=pygame.image.load("assets/aki sprite.png"), pos=(160, 65),
+                          text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
+        AKI_ICON.changeColor(OPTIONS_MOUSE_POS)
+        AKI_ICON.update(SCREEN)
 
+        DENJI_ICON = Button(image=pygame.image.load("assets/denji sprite.png"), pos=(80, 65),
+                          text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
+        DENJI_ICON.changeColor(OPTIONS_MOUSE_POS)
+        DENJI_ICON.update(SCREEN)
+
+        POWER_ICON = Button(image=pygame.image.load("assets/power sprite.png"), pos=(240, 65),
+                          text_input="", font=get_font(15), base_color="Black", hovering_color="Green")
+        POWER_ICON.changeColor(OPTIONS_MOUSE_POS)
+        POWER_ICON.update(SCREEN)
+        
         OPTIONS_TEXT = get_font(10).render("CHOOSE YOUR CHARACTERS.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(160, 65))
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(midtop=(160, 0))
+        
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
         OPTIONS_BACK = Button(image=None, pos=(160, 115), 
@@ -52,7 +69,7 @@ def options():
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
